654746476dad55ac4976ff5ef1372b8ddc823025
NeveSGG <neverovsg@gmail.com>
2022-10-29 21:28:32 +0600

    second commit second commit

--- src/utils/makeCardsList.tsx
+++ src/utils/makeCardsList.tsx
@@ -2,9 +2,7 @@ import { Card } from "../components/card/Card"
 import { getAddresses } from "../data/getAddresses"
 
 export function makeCardsList() {
-
     const cardsData = getAddresses()
-    
     const cards = cardsData.map((data) => 
         <Card 
             {... data}
